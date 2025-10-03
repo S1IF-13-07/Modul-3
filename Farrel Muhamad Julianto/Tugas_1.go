c507c65dfbc89cfc770d615c1e75de27737bf2d3
farrelmj0207-sketch <farrelmj0207@gmail.com>
2025-10-03 17:07:27 +0700

    Tugas_1.go

--- Farrel Muhamad Julianto/Tugas_1.go
+++ Farrel Muhamad Julianto/Tugas_1.go
@@ -1,41 +1,17 @@
 package main
 
-import (
-	"errors"
-	"fmt"
-	"math"
-)
-
-// Fungsi untuk menghitung f(x)=(2/(x+5))+5 dan membulatkan hasilnya ke bilangan bulat
-func calculateAndRound(x float64) (int, error) {
-	// Validasi untuk menghindari pembagian dengan nol
-	if x+5 == 0 {
-		return 0, errors.New("error: pembagian dengan nol karena x + 5 = 0")
-	}
-
-	// Lakukan perhitungan
-	result := (2 / (x + 5)) + 5
-
-	// Bulatkan hasil ke bilangan bulat terdekat dan ubah ke tipe int
-	roundedResult := int(math.Round(result))
-
-	return roundedResult, nil
-}
+import "fmt"
 
 func main() {
-	var x float64
+	var fx float64
 
-	fmt.Print("Masukkan nilai x: ")
-	_, err := fmt.Scan(&x)
-	if err != nil {
-		fmt.Println("Input tidak valid. Harap masukkan angka.")
-		return
-	}
+	// Input nilai f(x)
+	fmt.Print("Masukkan nilai f(x): ")
+	fmt.Scan(&fx)
 
-	roundedResult, err := calculateAndRound(x)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Printf("Hasil keluaran f(x)=(2/(x+5))+5 adalah: %d\n", roundedResult)
-	}
+	// Rumus: x = 2 / (f(x) - 5) - 5
+	x := (2 / (fx - 5)) - 5
+
+	// Tampilkan hasil
+	fmt.Println("Nilai x adalah: ", x)
 }
