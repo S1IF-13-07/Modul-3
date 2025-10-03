463069c42d556976c5e49e050716f504469f7ad3
IsmailMarasabesy <faizismail749@gmail.com>
2025-10-03 23:25:52 +0700

    Add files via upload

---
+++ ismailmarasabessy/soal32.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var r float64
+
+	fmt.Print("Jejari = ")
+	fmt.Scan(&r)
+
+	volume := (4.0 / 3.0) * math.Pi * math.Pow(r, 3)
+	luas := 4 * math.Pi * math.Pow(r, 2)
+
+	fmt.Printf("Bola dengan jejari %.2f memiliki volume %.4f dan luas permukaan %.4f\n", r, volume, luas)
+}
