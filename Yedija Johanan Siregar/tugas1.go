2bc8ebe06105014d545795b89950091f9a1ae91d
Yedija Johanan Siregar <115084766+YedijaJS@users.noreply.github.com>
2025-10-04 11:29:17 +0700

    Update and rename a to tugas1.go

---
+++ Yedija Johanan Siregar/tugas1.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+    var fx float64
+
+    // Input nilai f(x)
+    fmt.Print("Masukkan nilai f(x): ")
+    fmt.Scan(&fx)
+
+    // Rumus: x = 2 / (f(x) - 5) - 5
+    x := (2/(fx-5)) - 5
+
+    // Tampilkan hasil
+    fmt.Println("Nilai x adalah: ", x)
+}
