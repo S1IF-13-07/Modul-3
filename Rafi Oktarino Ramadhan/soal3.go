de9f94f3b31d8a075922c3b84119d9e5b3e67c80
rafioktars <152888181+rafioktars@users.noreply.github.com>
2025-10-05 15:31:34 +0700

    Create soal3.go

---
+++ Rafi Oktarino Ramadhan/soal3.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var tahun int
+	fmt.Print("Masukan Tahun : ")
+	fmt.Scan(&tahun)
+
+	kabisat := (tahun%400 == 0) || (tahun%4 == 0 && tahun%100 != 0)
+	fmt.Printf("Kabisat: %t\n", kabisat)
+
+}
