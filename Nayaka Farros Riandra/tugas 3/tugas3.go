d4a458822e669728f6c512c9238b64a5096bfcc9
Nayaka Farros Riandra <107793824+Nayakafarros@users.noreply.github.com>
2025-10-06 22:58:19 +0700

    Add files via upload

---
+++ Nayaka Farros Riandra/tugas 3/tugas3.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var tahun int
+
+	fmt.Printf("masukkan tahun = ")
+	fmt.Scan(&tahun)
+
+	if (tahun % 4 == 0 && tahun % 100 != 0) || (tahun %400 == 0) {
+		fmt.Printf("kabisat = ")
+		fmt.Println(true)
+	} else {
+		fmt.Printf("kabisat = ")
+		fmt.Println(false)
+	}
+}
